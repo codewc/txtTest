79e399b3aac68f1c2ec09c5d69a19112ec16ef88
wangchun <1164004116@qq.com>
2016-06-05 18:20:42 +0800

    我的

--- com/psn/wc/tool/CorePlanetForm.py
+++ com/psn/wc/tool/CorePlanetForm.py
@@ -3,31 +3,30 @@
 __author__ = '王春'
 
 import  ConfigParser
-class TxtTools(object):
-    def __init__(self):
+import  logging
+
+"""
+def getlogger():
+    logger = logging.getLogger('G'):
+    return logger;
+def get_config():
         config = ConfigParser.ConfigParser()
         with open("mycfg.cfg") as cfg:
             config.readfp(cfg)
-            self.path = config.get("info","path")
-            self.savepath = config.get("info","save_path")
-            print(self.savepath)
-        self.index = 0
-        self.mesage = "no message!!"
+            path = config.get("info","path")
+            savepath = config.get("info","save_path")
         print("初始化完成")
-    def readBook(self):
-        self.savef = open(self.savepath,"w")
-        try:
-            f = open(self.path,"r")
-            s = f.readlines()
-            for k in s:
-                pass
-        except:
-            print("出错了")
-        finally:
-            f.close()
-            self.savef.close()
+        return config
+"""
+config = None
+class ConfigUser():
+    def __init__(self):
+        self.config = ConfigParser.ConfigParser()
+        with open("mycfg.cfg") as cfg:
+            config = self.config
+            config.readfp(cfg)
+            path = config.get("info","path")
+            print(path)
 
-c = TxtTools()
-print(c.index)
-print(c.path)
-c.readBook()
+config = ConfigUser().config
+print(config)
